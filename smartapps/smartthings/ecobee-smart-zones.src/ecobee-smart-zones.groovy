4d7ff543bad74bcb87deb8d84230c68de2b53882
Barry Burke <SANdood@users.noreply.github.com>
2018-02-08 16:26:06 -0500

    v1.2.90- Final release before 1.3.0 transition

--- smartapps/smartthings/ecobee-smart-zones.src/ecobee-smart-zones.groovy
+++ smartapps/smartthings/ecobee-smart-zones.src/ecobee-smart-zones.groovy
@@ -17,9 +17,10 @@
  *	1.0.2 - Updated settings and TempDisable handling
  *  1.2.0 - Sync version number with new holdHours/holdAction support
  *	1.2.1 - Protect against LOG type errors
+ *	1.2.90- Final release before 1.3.0 transition
  *
  */
-def getVersionNum() { return "1.2.1" }
+def getVersionNum() { return "1.2.90" }
 private def getVersionLabel() { return "ecobee smartZones Version ${getVersionNum()}" }
 
 definition(
